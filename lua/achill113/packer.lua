92692500f1a7c37aed04869b9187ada114856746
Alex Hill <achill113@gmail.com>
2024-03-21 10:29:59 -0400

    feat: add ts tools

--- lua/achill113/packer.lua
+++ lua/achill113/packer.lua
@@ -71,4 +71,12 @@ return require('packer').startup(function(use)
           }
     }
 
+    use {
+      "pmizio/typescript-tools.nvim",
+      requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+      config = function()
+        require("typescript-tools").setup {}
+      end,
+    }
+
 end)
